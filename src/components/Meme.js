d2d0c2a587a0e6c93d8b2a72c037e88bcf8fa66f
Victor Quan <victor@Victors-MacBook-Pro.local>
2024-02-06 00:21:56 -0500

    Memes can't display without API key first

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,7 +1,28 @@
+import React from 'react';
+import memesData from "../memesData";
+
 export default function Meme() {
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: ""
+    });
+
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+
+    function getMemeImage() {
+        const memesArray = allMemeImages.data.memes;
+        const randomNumber = Math.floor(Math.random() * memesArray.length);
+        const url = memesArray[randomNumber].url;
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }));
+    };
+
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input 
                     type="text" 
                     placeholder="Top Text"
@@ -12,8 +33,9 @@ export default function Meme() {
                     placeholder="Bottom Text"
                     className="form--input"
                 />
-                <button className="form--button">Get a new meme image</button>
-            </form>
+                <button onClick={getMemeImage} className="form--button">Get a new meme image</button>
+            </div>
+            <img className="meme--image" src={meme.randomImage} alt="Meme"/>
         </main>
     )
 }
---
+++ src/memesData.js
@@ -0,0 +1,17 @@
+const memesData = {
+    "success": true,
+    "data" : {
+        "memes": [
+            {
+                "id": "181913649",
+                "name": "Drake Hotline Bling",
+                "url": "https://i.imgflip.com/30blgx.jpg",
+                "width": 1200,
+                "height": 1200,
+                "box_count": 2
+            }
+        ]
+    }
+}
+
+export default memesData;
