a59404397a29be64eac9b90bec41f0bcd3fb24a6
Victor Quan <victor@Victors-MacBook-Pro.local>
2024-02-05 21:41:02 -0500

    First Commit

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header className="header">
+            <h2>Header component</h2>
+            <h4>React Course - Project 3</h4>
+        </header>
+    )
+}
