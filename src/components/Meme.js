6d629469baa7615dbba401a10e682389e867102d
Victor Quan <victor@Victors-MacBook-Pro.local>
2024-02-06 14:19:56 -0500

    Implemented interactive input using hooks and state

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -14,11 +14,21 @@ export default function Meme() {
         const memesArray = allMemeImages.data.memes;
         const randomNumber = Math.floor(Math.random() * memesArray.length);
         const url = memesArray[randomNumber].url;
+
         setMeme(prevMeme => ({
             ...prevMeme,
             randomImage: url
         }));
     };
+
+    function handleChange(event) {
+        const {name, value} = event.target;
+
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name] : value
+        }))
+    }
 
     return (
         <main>
@@ -27,15 +37,25 @@ export default function Meme() {
                     type="text" 
                     placeholder="Top Text"
                     className="form--input"
+                    name="topText"
+                    value={meme.topText}
+                    onChange={handleChange}
                 />
                 <input 
                     type="text" 
                     placeholder="Bottom Text"
                     className="form--input"
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handleChange}
                 />
                 <button onClick={getMemeImage} className="form--button">Get a new meme image</button>
             </div>
-            <img className="meme--image" src={meme.randomImage} alt="Meme"/>
+            {meme.randomImage && <div className="meme">
+                <img className="meme--image" src={meme.randomImage} alt="Meme"/>
+                <h2 className="meme--top">{meme.topText}</h2>
+                <h2 className="meme--bottom">{meme.bottomText}</h2>
+            </div>}
         </main>
     )
 }
--- src/memesData.js
+++ src/memesData.js
@@ -5,11 +5,43 @@ const memesData = {
             {
                 "id": "181913649",
                 "name": "Drake Hotline Bling",
-                "url": "https://i.imgflip.com/30blgx.jpg",
+                "url": "https://placehold.co/1200x600",
                 "width": 1200,
-                "height": 1200,
+                "height": 600,
                 "box_count": 2
-            }
+            },
+            {
+                "id": "987654321",
+                "name": "Grumpy Cat",
+                "url": "https://placehold.co/800x600",
+                "width": 800,
+                "height": 600,
+                "box_count": 1
+            },
+            {
+                "id": "123456789",
+                "name": "Success Kid",
+                "url": "https://placehold.co/500x300",
+                "width": 500,
+                "height": 300,
+                "box_count": 1
+            },
+            {
+                "id": "456789123",
+                "name": "Philosoraptor",
+                "url": "https://placehold.co/1024x768",
+                "width": 1024,
+                "height": 768,
+                "box_count": 1
+            },
+            {
+                "id": "246813579",
+                "name": "Bad Luck Brian",
+                "url": "https://placehold.co/600x400",
+                "width": 600,
+                "height": 400,
+                "box_count": 1
+            },            
         ]
     }
 }
